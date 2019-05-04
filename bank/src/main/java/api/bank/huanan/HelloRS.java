9d21d2e63664deba2819af16c6c9503648f7de3d
ideasiii <louisju@iii.org.tw>
2019-05-04 22:37:34 +0800

    update_jugo

--- bank/src/main/java/api/bank/huanan/HelloRS.java
+++ bank/src/main/java/api/bank/huanan/HelloRS.java
@@ -1,12 +1,16 @@
 package api.bank.huanan;
 
+import org.json.simple.JSONObject;
+
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
 import api.modules.SqliteHandler;
 
@@ -44,5 +48,85 @@ public class HelloRS
             
         }
         return "Hello, " + name;
+    }
+    
+    @POST
+    @Produces("text/html") // content type to output
+    @Path("/account")
+    public String bank_account(@QueryParam("id") String id, @QueryParam("token") String token,
+            @Context HttpServletRequest request)
+    {
+        String strResponse = "{\"message\":\"parameter error!!\"}";
+        String strRecord;
+        JSONObject jsonObject;
+        
+        if (id != null && !id.equals("") && token != null && !token.equals(""))
+        {
+            try
+            {
+                SqliteHandler sqliteHandler = new SqliteHandler();
+                Connection conn = sqliteHandler.getConnection("database/huanan.db");
+                if (conn != null)
+                {
+                    String sql = "select * from bank_account where id = '" + id + "'";
+                    Statement stat = null;
+                    ResultSet rs = null;
+                    stat = conn.createStatement();
+                    rs = stat.executeQuery(sql);
+                    jsonObject = new JSONObject();
+                    if (rs.next())
+                    {
+                        jsonObject.put("id", rs.getInt("id"));
+                        jsonObject.put("balance", rs.getInt("balance"));
+                        jsonObject.put("birthday", rs.getString("birthday"));
+                        jsonObject.put("career", rs.getString("career"));
+                        jsonObject.put("residence", rs.getString("residence"));
+                        jsonObject.put("income", rs.getInt("income"));
+                        jsonObject.put("service_units", rs.getString("service_units"));
+                        jsonObject.put("marital", rs.getString("marital"));
+                        jsonObject.put("education", rs.getString("education"));
+                        jsonObject.put("dependents", rs.getInt("dependents"));
+                        jsonObject.put("balance_update_date", rs.getString("balance_update_date"));
+                        jsonObject.put("create_date", rs.getString("create_date"));
+                        return jsonObject.toString();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+            }
+            return "account id is " + id + " token is " + token;
+        }
+        
+        return strResponse;
+    }
+    
+    //=========================   Table Information ============================================//
+    
+    /*TABLE "bank_account" (
+	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"balance"	INTEGER NOT NULL DEFAULT 0,
+	"birthday"	TEXT NOT NULL,
+	"career"	TEXT NOT NULL,
+	"residence"	TEXT,
+	"income"	INTEGER,
+	"service_units"	TEXT,
+	"marital"	TEXT,
+	"education"	TEXT,
+	"dependents"	INTEGER,
+	"balance_update_date"	TEXT,
+	"create_date"	TEXT DEFAULT CURRENT_TIMESTAMP
+)*/
+    
+    @POST
+    @Produces("text/html") // content type to output
+    @Path("/trans_record")
+    public String bank_account(@QueryParam("id") String id, @QueryParam("token") String token,
+            @Context HttpServletRequest request)
+    {
+        String strResponse = "{\"message\":\"parameter error!!\"}";
+        String strRecord;
+        JSONObject jsonObject;
     }
 }
