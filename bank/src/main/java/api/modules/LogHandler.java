24c38937f9a4fd7ecc00db4efa95f2cfe89b8a9f
ideasiii <louisju@iii.org.tw>
2019-06-17 11:24:17 +0800

    update_jugo

--- bank/src/main/java/api/modules/LogHandler.java
+++ bank/src/main/java/api/modules/LogHandler.java
@@ -19,30 +19,57 @@ public abstract class LogHandler
 {
    public static void log(String strToken, HttpServletRequest request)
    {
-       Date dNow = new Date( );
-       SimpleDateFormat ft =
-               new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss");
-       
-       MongoClient mongoClient = new MongoClient("localhost", 27017);
-       MongoDatabase database = mongoClient.getDatabase("trackDB");
-       MongoCollection<Document> collection = database.getCollection("track");
-       Document document = new Document();
-       Document doc_url = new Document();
-       doc_url.put("url", request.getRequestURL().toString());
-       doc_url.put("param", request.getQueryString());
-       doc_url.put("method", request.getMethod());
-       doc_url.put("remote", request.getRemoteAddr());
-       document.put("token", strToken);
-       document.put("request",doc_url);
-       document.put("create_date",ft.format(dNow));
-       collection.insertOne(document);
-    
-       doc_url = null;
-       document = null;
-       mongoClient.close();
-       mongoClient = null;
+       LogThread logThread = new LogThread(strToken,request.getRequestURL().toString(),
+               request.getQueryString(),request.getMethod(),request.getRemoteAddr());
+       logThread.start();
    }
-   
     
+    static class LogThread extends Thread {
+       
+       private String m_strToken;
+       private String m_strURL;
+       private String m_strParam;
+       private String m_strMethod;
+       private String m_strRemove;
+       
+       LogThread(String strToken, String strURL, String strParam, String strMethod,
+               String strRemove)
+       {
+           m_strToken = strToken;
+           m_strURL = strURL;
+           m_strParam = strParam;
+           m_strMethod = strMethod;
+           m_strRemove = strRemove;
+       }
+        public void run(){
+            try
+            {
+                MongoClient mongoClient = new MongoClient("localhost", 27017);
+                MongoDatabase database = mongoClient.getDatabase("trackDB");
+                MongoCollection<Document> collection = database.getCollection("track");
+                Document document = new Document();
+                Document doc_url = new Document();
+                Date dNow = new Date();
+                SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                doc_url.put("url", m_strURL);
+                doc_url.put("param", m_strParam);
+                doc_url.put("method", m_strMethod);
+                doc_url.put("remote", m_strRemove);
+                document.put("token", m_strToken);
+                document.put("request", doc_url);
+                document.put("create_date", ft.format(dNow));
+                collection.insertOne(document);
+        
+                doc_url = null;
+                document = null;
+                mongoClient.close();
+                mongoClient = null;
+            }
+            catch(Exception e)
+            {
+                System.out.println(e.toString());
+            }
+        }
+    }
    
 }
