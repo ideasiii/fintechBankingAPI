cf3d85d91190be846f77ec83bd10605eaa055bb3
anny <anny10144@gmail.com>
2019-06-24 17:03:50 +0800

    add payments api

--- bank/src/main/java/api/bank/huanan/insurance/insurance.java
+++ bank/src/main/java/api/bank/huanan/insurance/insurance.java
@@ -422,4 +422,96 @@ public class insurance
         
     }
     
+    @GET
+    @Path("/payments")
+    public String payments(@QueryParam("user_id") int id,
+            @QueryParam("api_key") String token, @Context HttpServletRequest request)
+    {
+        LogHandler.log(token, request);
+        JSONObject jsonObject, dataJson;
+        JSONArray jsonArray;
+        jsonObject = new JSONObject();
+        
+        if (id != 0 && token != null && !token.equals(""))
+        {
+            
+            try
+            {
+                
+                SqliteHandler sqliteHandler = new SqliteHandler();
+                Connection conn = sqliteHandler.getConnection("database/huanan.db");
+                
+                if (conn != null)
+                {
+                    String sql =
+                            "select * from payment_method where user_id =" + id ;
+                    Statement stat = null;
+                    ResultSet rs = null;
+                    stat = conn.createStatement();
+                    rs = stat.executeQuery(sql);
+                    jsonArray = new JSONArray();
+                    int active = 0;
+                    
+                    
+                    if (rs.next())
+                    {
+                        do
+                        {
+                            dataJson = new JSONObject();
+                            dataJson.put("id", rs.getString("id"));
+                            dataJson.put("policy_no", rs.getString("policy_no"));
+                            dataJson.put("pay_status", rs.getInt("pay_status"));
+                            dataJson.put("pay_date", rs.getString("pay_date"));
+                            dataJson.put("pay_category", rs.getString("pay_category"));
+                            dataJson.put("payments_period", rs.getString("payments_period"));
+                            dataJson.put("create_date", rs.getString("create_date"));
+                            
+                            jsonArray.put(dataJson);
+                            
+                        } while (rs.next());
+                        
+                        jsonObject.put("user_id", id);
+                        jsonObject.put("payments_record", jsonArray);
+                        return jsonObject.toString();
+                        
+                    }
+                    else
+                    {
+                        jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_NO_RECORD_CODE);
+                        jsonObject.put("ERROR_MESSAGE", ErrorHandler.ERROR_NO_RECORD);
+                        return jsonObject.toString();
+                    }
+                    
+                }
+                else
+                {
+                    System.out.println("Database Connect Fail");
+                    jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_CONNECT_DB_CODE);
+                    jsonObject.put("ERROR_MESSAGE", ErrorHandler.ERROR_CONNECT_DB);
+                    return jsonObject.toString();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+                
+                jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_EXCEPTION);
+                jsonObject.put("ERROR_MESSAGE", e.getMessage());
+                
+                return jsonObject.toString();
+                
+            }
+            
+            
+        }
+        
+        
+        jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_PARM_CODE);
+        jsonObject.put("ERROR_MESSAGE", ErrorHandler.ERROR_PARM);
+        return jsonObject.toString();
+        
+        
+    }
+    
 }
