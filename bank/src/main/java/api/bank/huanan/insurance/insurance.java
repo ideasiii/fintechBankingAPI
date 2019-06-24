c6d91dfb6f9c0ab1a6dd645809a56f1c4f524e92
anny <anny10144@gmail.com>
2019-06-24 15:02:17 +0800

    add expiration api

--- bank/src/main/java/api/bank/huanan/insurance/insurance.java
+++ bank/src/main/java/api/bank/huanan/insurance/insurance.java
@@ -217,4 +217,100 @@ public class insurance
         
     }
     
+    @GET
+    @Path("/expiration")
+    public String expiration(@QueryParam("identity_id") String identity_id,
+            @QueryParam("api_key") String token, @Context HttpServletRequest request)
+    {
+        LogHandler.log(token, request);
+        JSONObject jsonObject, dataJson;
+        JSONArray jsonArray;
+        jsonObject = new JSONObject();
+        
+        if (identity_id != null && !identity_id.equals("") && token != null && !token.equals(""))
+        {
+            
+            try
+            {
+                
+                SqliteHandler sqliteHandler = new SqliteHandler();
+                Connection conn = sqliteHandler.getConnection("database/huanan.db");
+                
+                if (conn != null)
+                {
+                    String sql =
+                            "select * from insurance_exp_date where identity_id ='" + identity_id +
+                            "'";
+                    Statement stat = null;
+                    ResultSet rs = null;
+                    stat = conn.createStatement();
+                    rs = stat.executeQuery(sql);
+                    jsonArray = new JSONArray();
+                    int active = 0;
+                    
+                    
+                    if (rs.next())
+                    {
+                        do
+                        {
+                            dataJson = new JSONObject();
+                            dataJson.put("id", rs.getString("id"));
+                            dataJson.put("policy_no", rs.getString("policy_no"));
+                            dataJson.put("exp_date", rs.getString("exp_date"));
+                            dataJson.put("is_active",rs.getInt("is_active"));
+                            
+                            jsonArray.put(dataJson);
+                            
+                            if(rs.getInt("is_active") == 1){
+                                active = 1;
+                            }
+                            
+                            
+                        } while (rs.next());
+                        
+                        jsonObject.put("identity_id", identity_id);
+                        jsonObject.put("is_active_user", active);
+                        jsonObject.put("exp_record", jsonArray);
+                        return jsonObject.toString();
+                        
+                    }
+                    else
+                    {
+                        jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_NO_RECORD_CODE);
+                        jsonObject.put("ERROR_MESSAGE", ErrorHandler.ERROR_NO_RECORD);
+                        return jsonObject.toString();
+                    }
+                    
+                }
+                else
+                {
+                    System.out.println("Database Connect Fail");
+                    jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_CONNECT_DB_CODE);
+                    jsonObject.put("ERROR_MESSAGE", ErrorHandler.ERROR_CONNECT_DB);
+                    return jsonObject.toString();
+                }
+                
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+                
+                jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_EXCEPTION);
+                jsonObject.put("ERROR_MESSAGE", e.getMessage());
+                
+                return jsonObject.toString();
+                
+            }
+            
+            
+        }
+        
+        
+        jsonObject.put("ERROR_CODE", ErrorHandler.ERROR_PARM_CODE);
+        jsonObject.put("ERROR_MESSAGE", ErrorHandler.ERROR_PARM);
+        return jsonObject.toString();
+        
+        
+    }
+    
 }
