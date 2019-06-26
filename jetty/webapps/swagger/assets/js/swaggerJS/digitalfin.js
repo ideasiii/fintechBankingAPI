c17637c40370b4bee2f57a17ae21a30fe3351e4a
anny <anny10144@gmail.com>
2019-06-26 11:53:05 +0800

    update swagger

--- jetty/webapps/swagger/assets/js/swaggerJS/digitalfin.js
+++ jetty/webapps/swagger/assets/js/swaggerJS/digitalfin.js
@@ -1,6 +1,6 @@
 $(function () {
 	
-	url = "http://54.92.0.255/swaggerJson/digitalfin_v1.json";
+	url = "http://54.92.0.255/swaggerJson/digitalfin_v2.json";
 	
 	const ui = SwaggerUIBundle({
 		url: url,
