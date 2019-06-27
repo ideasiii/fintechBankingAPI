24617fe8eebc27affd14d01385b2cfd5d6f99766
anny <anny10144@gmail.com>
2019-06-27 11:38:58 +0800

    upload fund swagger json

---
+++ jetty/webapps/swagger/assets/js/swaggerJS/fund.js
@@ -0,0 +1,26 @@
+$(function () {
+	
+	url = "http://54.92.0.255/swaggerJson/fund_v2.json";
+	
+	const ui = SwaggerUIBundle({
+		url: url,
+		validatorUrl : null,
+		dom_id: '#swagger-ui',
+		deepLinking: true,
+		presets: [
+					SwaggerUIBundle.presets.apis,
+					SwaggerUIStandalonePreset
+				],
+		plugins: [
+					SwaggerUIBundle.plugins.DownloadUrl,
+					HideTopbarPlugin,
+					HideInfoUrlPlugin
+				],
+		layout: "StandaloneLayout",
+		docExpansion: "list"
+		
+	})
+
+	window.ui = ui
+})
+
