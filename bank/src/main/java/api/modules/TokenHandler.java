d665b3e8d2e4e9011e8cbc61fe2d48b1ae5975c6
ideasiii <louisju@iii.org.tw>
2019-07-01 13:59:06 +0800

    add_token

---
+++ bank/src/main/java/api/modules/TokenHandler.java
@@ -0,0 +1,22 @@
+package api.modules;
+
+/**
+ * Created by Jugo on 2019/6/27
+ */
+
+
+class TokenHandler
+{
+    public TokenHandler()
+    {
+    }
+    
+    /**
+     * 產生token新增到資料庫
+     * @param count 產生的筆數
+     */
+    public void genericToken(int count)
+    {
+
+    }
+}
