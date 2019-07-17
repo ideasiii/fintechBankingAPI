27170ef5e9611da5040aa8dcff7a7bc71039d9c0
anny <anny10144@gmail.com>
2019-07-17 15:11:38 +0800

    update token api

--- jetty/webapps/tokenManager/assets/js/tokenlist.js
+++ jetty/webapps/tokenManager/assets/js/tokenlist.js
@@ -115,7 +115,7 @@ function list() {
 						cell.data("").draw;
 					}else{
 						$.ajax({
-							url: 'http://http://www.fintechersapi.com/bank/huanan/token/list/' + rowID,
+							url: 'http://www.fintechersapi.com/bank/huanan/token/list/' + rowID,
 							type: "PATCH",
 							dataType: 'json',
 							contentType: "application/json",
@@ -147,7 +147,7 @@ function list() {
 		mounted() {
 			let vm = this;
 			$.ajax({
-				url: 'http://localhost:8080/bank/huanan/token/list',
+				url: 'http://www.fintechersapi.com/bank/huanan/token/list',
 				type: "GET",
 				dataType: 'json',
 				statusCode: {
