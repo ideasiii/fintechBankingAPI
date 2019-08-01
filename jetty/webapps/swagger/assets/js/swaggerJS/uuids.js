6088cc3190608fe4ab84ecb546018e20fef60858
anny <anny10144@gmail.com>
2019-08-01 20:21:50 +0800

    add uuids page

---
+++ jetty/webapps/swagger/assets/js/swaggerJS/uuids.js
@@ -0,0 +1,25 @@
+$(function () {
+	
+	url = "https://www.fintechersapi.com/swaggerJson/uuids.json";
+	
+	const ui = SwaggerUIBundle({
+		url: url,
+		validatorUrl : null,
+		dom_id: '#swagger-ui',
+		deepLinking: true,
+		presets: [
+					SwaggerUIBundle.presets.apis,
+					SwaggerUIStandalonePreset
+				],
+		plugins: [
+					SwaggerUIBundle.plugins.DownloadUrl,
+					HideTopbarPlugin,
+					HideInfoUrlPlugin
+				],
+		layout: "StandaloneLayout",
+		docExpansion: "list"
+		
+	})
+
+	window.ui = ui
+})
