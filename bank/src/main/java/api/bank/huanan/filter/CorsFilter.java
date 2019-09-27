f20593c9f92f43726c4a04ba7193fb1330efeb18
anny <anny10144@gmail.com>
2019-09-27 10:36:43 +0800

    cors test

--- bank/src/main/java/api/bank/huanan/filter/CorsFilter.java
+++ bank/src/main/java/api/bank/huanan/filter/CorsFilter.java
@@ -3,8 +3,10 @@ package api.bank.huanan.filter;
 import java.io.IOException;
 
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
 /**
@@ -12,8 +14,32 @@ import javax.ws.rs.ext.Provider;
  */
 
 @Provider
-public class CorsFilter implements ContainerResponseFilter
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter
 {
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {//Request Filter
+        
+        requestContext.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
+        requestContext.getHeaders().add( "Access-Control-Allow-Origin", "*");
+        requestContext.getHeaders().add( "Access-Control-Allow-Methods", "OPTIONS, GET, POST, DELETE, PUT" );
+        requestContext.getHeaders().add( "Access-Control-Allow-Headers", "Content-Type" );
+        
+        // When HttpMethod comes as OPTIONS, just acknowledge that it accepts...
+        if ( requestContext.getRequest().getMethod().equals( "OPTIONS" ) ) {
+            
+            
+            // Just send a OK signal back to the browser (Abort the filter chain with a response.)
+            Response response = Response.status( Response.Status.OK )
+                    .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                    .header("Access-Control-Allow-Headers", "Content-Type, accept, headers")
+                    .build();
+            requestContext.abortWith( response );
+            
+        }
+        
+    }
+    
     
     @Override
     public void filter(ContainerRequestContext requestContext,
